3be2f71f93cc776956c2e485d523c94aff3ae7d4
William Stocks <willstocks@me.com>
2023-07-03 16:19:45 +0000

    Add getResult function to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,21 @@ function getComputerChoice () {
     const randomChoice = Math.floor(Math.random() * 3)
     return vldChoice[randomChoice]
 }
+
+// getResult compares playerChoice & computerChoice & return the score accordingly
+
+function getResult(playerChoice, computerChoice) {
+    let score;
+    if (playerChoice == computerChoice) {
+        score = 0
+    }   else if (playerChoice == 'Viking' && computerChoice == 'Dino') {
+        score = 1
+    }   else if (playerChoice == 'Luka' && computerChoice == 'Viking') {
+        score = 1
+    }   else if (playerChoice == 'Dino' && computerChoice == 'Luka') {
+        score = 1
+    }   else {
+        score = -1
+    }
+    return score
+}
