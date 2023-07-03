72cda5c1e4fba02d5d0213fe3bc2e8b152889030
William Stocks <willstocks@me.com>
2023-07-03 15:59:42 +0000

    Add getComputerChoice function to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("hello");
+// getComputerChoice randomly selects between `viking` luka` `dino` and returns that string
+
+function getComputerChoice () {
+    const vldChoice = ['Viking', 'Luka', 'Dino']
+    const randomChoice = Math.floor(Math.random() * 3)
+    return vldChoice[randomChoice]
+}
